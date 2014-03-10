a013fd2487aff909e1ab1fba51233f734fa8cf19
xuanyi <xuanxiang@xuanyiMBA.local>
2014-03-10 17:16:40 +0800

    add MaxTemperature.java

---
+++ MaxTemperature/MaxTemperature.java
@@ -0,0 +1,45 @@
+package csd.bupt;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+public class MaxTemperatureMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+        private static final int MISSING = 9999;
+
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+
+                String line = value.toString();
+                String year = line.substring(15, 19);
+                int airTemperature;
+                if(line.charAt(87) == '+' ){
+                        airTemperature = Integer.parseInt(line.substring(88, 92));
+                }
+                else {
+                        airTemperature = Integer.parseInt(line.substring(87, 92));
+                }
+                String quality = line.substring(92, 93);
+                if( airTemperature != MISSING && quality.matches("[01459]")){
+                        ouput.collect(new Text(year), new IntWritable(airTemperature));
+                }
+        }
+        
+
+}
+public class MaxTemperatureReducer extends MapReduceBase implements Reducer<Text IntWritable, Text, IntWritable> {
+
+        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+                int maxValue = Integer.MIN_VALUE;
+                while(values.hasNext()){
+                        maxValue = Math.max(maxValue, values.next().get());
+                }
+                output.collect(key, new IntWritable(maxValue));
+        }
+
+}
